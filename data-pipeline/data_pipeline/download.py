6192eca88d2b6b37072a5d4045fe84160be83f97
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-01-05 18:35:24 +0000

    added code to create a customer id for each transaction in both the train and test datasets

--- data-pipeline/data_pipeline/download.py
+++ data-pipeline/data_pipeline/download.py
@@ -9,8 +9,8 @@ transaction data feeding to the trained model using Kafka.
 Date: 2025-01-04
 """
 
-
 import os
+from hashlib import sha256
 from pathlib import Path
 
 import kaggle
@@ -20,12 +20,30 @@ from rich import print
 
 
 def extract_from_kaggle() -> None:
+    """
+    Main function to download the dataset from kaggle, convert to parquet,
+    and upload to s3.
+    """
+    # download data from kaggle
     _download_data()
 
-    paths = Path("data").glob("*.csv")
+    orig_paths = Path("data").glob("*.csv")
 
-    for path in paths:
+    for path in orig_paths:
         _convert_to_parquet(path)
+
+    # add customer id to both datasets
+    train_df = pl.read_parquet("data/fraudTrain.parquet")
+    test_df = pl.read_parquet("data/fraudTest.parquet")
+
+    sensitive_cols = ["first", "last", "cc_num", "dob"]
+
+    train_data, test_data = _generate_customer_id(
+        train_df=train_df, test_df=test_df, sensitive_cols=sensitive_cols
+    )
+
+    train_data.write_parquet("data/fraudTrain.parquet")
+    test_data.write_parquet("data/fraudTest.parquet")
 
     paths = [
         ("data/fraudTrain.parquet", "raw/training.parquet"),
@@ -55,6 +73,54 @@ def _convert_to_parquet(path: str | Path) -> None:
 
     output = str(path).replace(".csv", ".parquet")
     pl.read_csv(path).write_parquet(file=output)
+
+
+def _generate_customer_id(
+    train_df: pl.DataFrame, test_df: pl.DataFrame, sensitive_cols: list[str]
+) -> tuple[pl.DataFrame, pl.DataFrame]:
+    print("generating customer id")
+
+    def __create_customer_id(row: dict) -> str:
+        identifier = (
+            f"{str(row['first']).lower().strip()}"
+            f"{str(row['last']).lower().strip()}"
+            f"{str(row['cc_num']).strip()}"
+            f"{str(row['dob'])}"
+        )
+        # Create deterministic hash
+        return f"CUST_{sha256(identifier.encode()).hexdigest()[:16]}"
+
+    print("creating concatenating train and test data")
+    full_data = pl.concat(
+        [
+            train_df.with_columns(pl.lit("train").alias("original_split")),
+            test_df.with_columns(pl.lit("test").alias("original_split")),
+        ]
+    )
+
+    print('mapping "create_customer_id" function to sensitive columns')
+    processed_data = full_data.with_columns(
+        pl.struct(sensitive_cols)
+        .map_elements(lambda x: __create_customer_id(x), return_dtype=pl.String)
+        .alias("customer_id")
+    )
+
+    # split data hack into train and test
+    print("splitting data back into train and test")
+    train_data = processed_data.filter(pl.col("original_split") == "train").drop(
+        "original_split"
+    )
+    test_data = processed_data.filter(pl.col("original_split") == "test").drop(
+        "original_split"
+    )
+
+    print(
+        'asserting "train_data" and "test_data" have the same length as "train_df" and "test_df"'
+    )
+    assert len(train_df) == len(train_data)
+    assert len(test_df) == len(test_data)
+
+    return train_data, test_data
 
 
 def _upload_to_s3(bucket: str, input: str, output: str) -> None:
