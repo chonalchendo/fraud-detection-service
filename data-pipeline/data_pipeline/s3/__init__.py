c71664a150c1a5b33f74bd4640453cf5d0fae801
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-01-05 20:37:56 +0000

    added read_parquet

--- data-pipeline/data_pipeline/s3/__init__.py
+++ data-pipeline/data_pipeline/s3/__init__.py
@@ -1,4 +1,4 @@
-from .read import read_bucket
-from .write import write_bucket
+from .read import read_parquet
+from .write import write_deltalake, write_parquet
 
-__all__ = ["read_bucket", "write_bucket"]
+# __all__ = ["read_bucket", "write_deltalake", "write_parquet", "read_parquet"]
