c858c1634f5b8c6c8d5ed0cdf5f944cfb22a9263
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-01-06 19:50:50 +0000

    added pandera data model validator class to validate data

---
+++ data-pipeline/data_pipeline/validate.py
@@ -0,0 +1,26 @@
+import pandera.polars as pa
+from pandera.engines.polars_engine import Categorical, DateTime, Float64, Int64, String
+
+
+class ValidationModel(pa.DataFrameModel):
+    transaction_time: DateTime = pa.Field(nullable=False)
+    merchant_name: String = pa.Field(nullable=False)
+    category: Categorical = pa.Field(nullable=False)
+    amount_usd: Float64 = pa.Field(nullable=False)
+    gender: String = pa.Field(nullable=False)
+    street: String = pa.Field(nullable=False)
+    city: String = pa.Field(nullable=False)
+    state: String = pa.Field(nullable=False)
+    zip: Int64 = pa.Field(nullable=False)
+    lat: Float64 = pa.Field(nullable=False)
+    long: Float64 = pa.Field(nullable=False)
+    city_pop: Int64 = pa.Field(nullable=False)
+    job: String = pa.Field(nullable=False)
+    trans_num: String = pa.Field(nullable=False)
+    unix_time: Int64 = pa.Field(nullable=False)
+    merch_lat: Float64 = pa.Field(nullable=False)
+    merch_long: Float64 = pa.Field(nullable=False)
+    is_fraud: Int64 = pa.Field(nullable=False, isin=[0, 1])
+    customer_id: String = pa.Field(
+        nullable=False, str_length=21, str_startswith="CUST_"
+    )
