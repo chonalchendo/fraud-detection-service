808b6afbf2d793e5ad4aefe93da95d6cdfdbe9a5
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-01-09 19:36:37 +0000

    added codeartifact variables to configure resources and iam policies

--- infra/main.tf
+++ infra/main.tf
@@ -4,6 +4,8 @@ provider "aws" {
 
 locals {
   bucket_name = "fraud-detection-system"
+  codeartifact_domain = "fraud-detection-domain"
+  codeartifact_repository = "python-packages"
   user_arn = ["arn:aws:iam::061051215402:user/fraud-detection-system",]
   tags = {
     project = "fraud-detection-system"
@@ -14,16 +16,20 @@ module "base" {
   source = "./base"
   bucket_name = local.bucket_name
   tags = local.tags
-  codeartifact_domain = "fraud-detection-domain"
-  codeartifact_repository = "python-packages"
+  codeartifact_domain = local.codeartifact_domain 
+  codeartifact_repository = local.codeartifact_repository 
 }
 
 module "iam" {
   source = "./iam"
-  name = "fraud-detection-system"
+  user_name = "fraud-detection-system"
   tags = local.tags
   bucket_name = module.base.bucket_name 
   bucket_arn = module.base.bucket_arn
   user_arn = local.user_arn
+  codeartifact_domain = module.base.codeartifact_domain
+  codeartifact_repository = module.base.codeartifact_repository
+  codeartifact_repo_arn = module.base.codeartifact_repo_arn
+  codeartifact_domain_arn = module.base.codeartifact_domain_arn
 }
 
